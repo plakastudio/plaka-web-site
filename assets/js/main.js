2bba0a7cdb0d7164675d0ba1b0fcb656a67df037
Fernando Blanco <fernando.blanco@sdggroup.com>
2020-01-07 02:37:20 +0100

    Reducido el metodo de contacto Borrados js innecesario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,58 @@
 $(function() {
-    
+
     "use strict";
-    
+
     //===== Prealoder
-    
+
     $(window).on('load', function(event) {
         $('.preloader').delay(500).fadeOut(500);
     });
-    
-    
-    //===== Sticky
-    
-    // grab an element
-    var myElement = document.querySelector(".headroom");
-    // construct an instance of Headroom, passing the element
-    var headroom  = new Headroom(myElement);
-    // initialise
-    headroom.init();
-    
-    
-    $('#nav').onePageNav({
-        currentClass: 'active',
-        changeHash: true,
-        scrollSpeed: 800,
-        scrollThreshold: 0.5,
-        filter: '',
-        easing: 'swing',
-        begin: function() {
-            //I get fired when the animation is starting
-        },
-        end: function() {
-            //I get fired when the animation is ending
-        },
-        scrollChange: function($currentListItem) {
-            //I get fired when you enter a section and I pass the list item of the section
-        }
-    });
-    
-    
-    $.scrollIt({
-        scrollTime: 800, 
-    });
-    
-        
+
+
     //===== close navbar-collapse when a  clicked
-    
+
     $(".navbar-nav a").on('click', function () {
         $(".navbar-collapse").removeClass("show");
     });
-    
-    
+
+
     //===== Mobile Menu
-    
+
     $(".navbar-toggler").on('click', function(){
         $(this).toggleClass("active");
     });
-    
+
     $(".navbar-nav a").on('click', function() {
         $(".navbar-toggler").removeClass('active');
     });
-    
+
     var subMenu = $(".sub-menu-bar .navbar-nav .sub-menu");
-    
+
     if(subMenu.length) {
         subMenu.parent('li').children('a').append(function () {
             return '<button class="sub-nav-toggler"> <span></span> </button>';
         });
-        
+
         var subMenuToggler = $(".sub-menu-bar .navbar-nav .sub-nav-toggler");
-        
+
         subMenuToggler.on('click', function() {
             $(this).parent().parent().children(".sub-menu").slideToggle();
             return false
         });
-        
+
     }
-    
-    
+
+
     //===== Counter Up
-    
+
     $('.counter').counterUp({
         delay: 10,
         time: 3000
     });
-    
-    
+
+
     //===== Isotope Project 1
-    
+
     $('.container').imagesLoaded(function () {
         var $grid = $('.grid').isotope({
         // options
@@ -94,17 +60,17 @@ $(function() {
             itemSelector: '.grid-item',
             percentPosition: true,
             masonry: {
-                
+
             }
-            
+
         });
-        
+
         // filter items on button click
         $('.project-menu ul').on( 'click', 'li', function() {
           var filterValue = $(this).attr('data-filter');
           $grid.isotope({ filter: filterValue });
         });
-        
+
         //for menu active class
         $('.project-menu ul li').on('click', function (event) {
             $(this).siblings('.active').removeClass('active');
@@ -112,10 +78,10 @@ $(function() {
             event.preventDefault();
         });
     });
-    
-    
+
+
     //===== Back to top
-    
+
     // Show or hide the sticky footer button
     $(window).on('scroll', function(event) {
         if($(this).scrollTop() > 600){
@@ -124,18 +90,18 @@ $(function() {
             $('.back-to-top').fadeOut(200)
         }
     });
-    
-    
+
+
     //Animate the scroll to yop
     $('.back-to-top').on('click', function(event) {
         event.preventDefault();
-        
+
         $('html, body').animate({
             scrollTop: 0,
         }, 1500);
     });
-    
-    
+
+
     //=====  services progress
 
     if($('.our-services-progress').length){
@@ -156,7 +122,7 @@ $(function() {
             });
         });
     }
-    
+
     if($('.our-services-progress').length){
         $('.our-services-progress').appear(function(){
             Circles.create({
@@ -175,7 +141,7 @@ $(function() {
             });
         });
     }
-    
+
     if($('.our-services-progress').length){
         $('.our-services-progress').appear(function(){
             Circles.create({
@@ -194,7 +160,7 @@ $(function() {
             });
         });
     }
-    
+
     if($('.our-services-progress').length){
         $('.our-services-progress').appear(function(){
             Circles.create({
@@ -213,10 +179,10 @@ $(function() {
             });
         });
     }
-   
-    
+
+
     //===== slick Testimonial
-    
+
     $('.testimonial-active').slick({
         dots: false,
         arrows: true,
@@ -228,24 +194,24 @@ $(function() {
         speed: 800,
         slidesToShow: 1,
     });
-    
-    
+
+
     //===== Magnific Popup
-    
+
     $('.image-popup').magnificPopup({
       type: 'image',
       gallery:{
         enabled:true
       }
     });
-    
-    
-    
-    
+
+
+
+
     //=====  WOW active
-    
+
     new WOW().init();
-    
-    
-    
+
+
+
 });
